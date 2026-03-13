e4c6946219be7f28e089d67503773c00cf4a37bb
devcaetan0 <davi_caetano@estudante.sesisenai.org.br>
2026-03-13 10:09:52 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// console.log("Caguei nas calças e estou borrado");
+/*
+console.log("Caguei nas calças e estou borrado");
 
-// let nome="Davi Batista";
-// console.log(nome);
+let nome="Davi Batista";
+console.log(nome);
 
-// const pi=3.14;
-// console.log(pi)
+const pi=3.14;
+console.log(pi)
 
-// let n1=67;
-// let n2=67;
-// console.log(n2+n1);
+let n1=67;
+let n2=67;
+console.log(n2+n1);
 
-// function saudacao(x,y){
-//     console.log("Olá, meu nome é "+nome);
-// };
+function saudacao(x,y){
+    console.log("Olá, meu nome é "+nome);
+};
 
-// function somar(x,y){
-//     console.log(x+y);
-// };
+function somar(x,y){
+    console.log(x+y);
+};
 
-// saudacao();
-// somar(21,21);
+saudacao();
+somar(21,21);
 
 function somar_numeros(){
     let soma=0;
@@ -32,7 +33,6 @@ function somar_numeros(){
             alert("Por favor, insira um número válido");
         }else{
             soma+=numero;
-            console.log(soma);
         }
         continuar=confirm("Deseja adicionar mais um número?");
     }
@@ -43,8 +43,28 @@ somar_numeros();
 
 function contagem_regressiva(){
     for(let i=10;i>0;i--){
-        alert(i);
+        console.log(i);
     }
-    alert("Lançamento realizado!");
+    console.log("Lançamento realizado!");
 }
 contagem_regressiva();
+*/
+
+function calculo_financeiro(){
+    let soma_ganhos=0;
+    let soma_gastos=0;
+
+    for(let i=0;i<12;i++){
+        let ganhos_mes=parseFloat(prompt("Informe o ganho bruto do "+(i+1)+"° mês: "));
+        let gastos_mes=parseFloat(prompt("Informe o gasto total do "+(i+1)+"° mês: "));
+
+        soma_ganhos+=ganhos_mes;
+        soma_gastos+=gastos_mes;
+    }
+    let saldo=soma_ganhos-soma_gastos;
+
+    alert("Ganho bruto anual: R$"+soma_ganhos);
+    alert("Gasto anual: R$"+soma_gastos);
+    alert("Saldo anual: R$"+saldo);
+}
+calculo_financeiro();
